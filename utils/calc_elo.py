2f63858eee8f670189072764545512570d17552d
sillypears <sillypears@users.noreply.github.com>
2025-08-07 16:11:22 -0400

    Clean this up, maybe figure out the various k values from the api

--- utils/calc_elo.py
+++ utils/calc_elo.py
@@ -11,7 +11,7 @@ def estimate_opponent_elo(my_elo: int, elo_change: int, result: int, k: int = 25
         my_elo (int): The original elo
         elo_change (int): Change from the match result
         result (int): 0/1 depending on match
-        k (int, optional): Algo change. Defaults to 25.
+        k (int, optional): Algo change. Defaults 25
 
     Raises:
         ValueError: If the expected elo
@@ -19,20 +19,23 @@ def estimate_opponent_elo(my_elo: int, elo_change: int, result: int, k: int = 25
     Returns:
         int: Opponent's guessed elo
     """
-        
+    k_values = {
+        "ranked_elo_start_value": 1000,
+        "ranked_elo_placement_k": 120,
+        "ranked_elo_postplacement_k": 40,
+        "ranked_elo_established_k": 24,
+        "ranked_placement_median_k": 80,
+        "ranked_placement_edge_k": 40,
+        "ranked_placement_median_elo": 900,
+        "ranked_placement_edge_difference": 450
+    }
     if (result == 1 and elo_change < 0) or (result == 0 and elo_change > 0):
         raise ValueError("Mismatch between match result and elo change sign.")
-    
     expected_score = result - (elo_change / k)
-
-    # Clamp expected score within (0, 1) to avoid log math domain errors
     epsilon = 1e-6
     expected_score = max(epsilon, min(1 - epsilon, expected_score))
-
-    # Calculate opponent Elo
     odds_ratio = (1 - expected_score) / expected_score
     opponent_elo = my_elo + 400 * math.log10(odds_ratio)
-
     return math.floor(opponent_elo)
 
 def main():
